69c7ecb58316ee006c05922f0f4c47afa86ac07e
K417s <80661104+K417s@users.noreply.github.com>
2021-05-24 12:35:39 +0800

    2021上機考v3_ (江俊陞)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,26 +14,51 @@ import kotlinx.android.synthetic.main.activity_main.*
 public final class MyAppGlideModule : AppGlideModule()
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val img: ImageView = findViewById(R.id.imageTitle)
-
+        val shape =
+            intArrayOf(R.drawable.circle, R.drawable.square, R.drawable.triangle, R.drawable.star)
+        val i: Int = (0..3).random()
+        imgNext.setImageResource(shape[i])
         GlideApp.with(this)
             .load(R.drawable.cover)
             .override(800, 600)
             .into(img)
 
-///
+
         Toast.makeText(baseContext, "作者：江俊陞", Toast.LENGTH_LONG).show()
-//
+
+
+
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(p0: View?): Boolean {
-                intent = Intent(this@MainActivity,GameAcitivity::class.java)
+                intent = Intent(this@MainActivity, GameAcitivity::class.java)
                 startActivity(intent)
                 return true
+
             }
         })
+
+        imgNext.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?){
+                imgNext.setImageResource(shape[i])
+                recreate()
+
+            }
+        })
+
+
+
+
     }
+
+
 }
